dc7801a70fd42e709758a2a547f207925ab86389
Md Ariful Islam <123arifulislam111@gmail.com>
2026-05-27 22:27:28 +0600

    Update project

--- studymart/About_Us/models.py
+++ studymart/About_Us/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    t_id = models.ImageField()
+    t_name = models.CharField(max_length=40)
+    t_email = models.EmailField(max_length=30)
--- studymart/Machine_Learning/urls.py
+++ studymart/Machine_Learning/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ml/',views.machine_learning),
-    path('rn/',views.rendom_forest),
-    path('kn/',views.k_nearest),
-    path('dt/',views.dtree),
+    path('ml/',views.machine_learning, name='ml'),
+    path('rn/',views.rendom_forest, name='rn'),
+    path('kn/',views.k_nearest,name='kn'),
+    path('dt/',views.dtree, name='dt'),
 ]
