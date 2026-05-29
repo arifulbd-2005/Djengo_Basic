af2b2b20e0462aaa665bc3ac4e2bcbca0c21e387
Md Ariful Islam <123arifulislam111@gmail.com>
2026-05-29 22:48:21 +0600

    Update project

--- studymart/Blogs/urls.py
+++ studymart/Blogs/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('b1/',views.blog1, name='blog1'),
-    path('b2/',views.blog2, name='blog2'),
+    path('b1/',views.blog1, name='b1'),
+    path('b2/',views.blog2, name='b2'),
+    path('fm/',views.showformsdata, name='fm'),
 ]
--- studymart/Blogs/views.py
+++ studymart/Blogs/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import TeachersRegistration
 
 # Create your views here.
 def blog1(request):
     return render(request, 'blogs/blog.html')
 def blog2(request):
     return render(request, 'blogs/blog2.html')
+def showformsdata(request):
+    fm = TeachersRegistration()
+    fm.order_fields(field_order=['email', 'first_name', 'last_name'])
+    return render(request, 'blogs/forms.html', {'form': fm})
