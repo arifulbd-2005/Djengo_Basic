6cf66813e90a0110dbc3039f143bd460b5bb7a75
Md Ariful Islam <123arifulislam111@gmail.com>
2026-05-31 11:04:40 +0600

    Update project

--- studymart/Blogs/forms.py
+++ studymart/Blogs/forms.py
@@ -1,10 +1,20 @@
 from django import forms
+import email
+from django.core import validators
 
 class TeachersRegistration(forms.Form):
     first_name = forms.CharField(label='Enter Your Frist Name', label_suffix=' ', initial='Studymart')
     last_name = forms.CharField(label='Enter Your Last Name')
     email = forms.EmailField(initial='studymart@gmail.com', disabled=True)
     password = forms.CharField(widget=forms.PasswordInput)
+    repassword = forms.CharField(widget=forms.PasswordInput)
     textarea = forms.CharField(widget=forms.Textarea)
     file = forms.CharField(widget=forms.FileInput)
     checkbox = forms.CharField(widget=forms.CheckboxInput)
+    
+    def clean(self):
+        self.cleaned_data = super().clean()
+        rightpass = self.cleaned_data['password']
+        wrongpass = self.cleaned_data['repassword']
+        if rightpass != wrongpass:
+            raise forms.ValidationError('Password does not match')
--- studymart/Blogs/views.py
+++ studymart/Blogs/views.py
@@ -10,11 +10,12 @@ def blog2(request):
 def showformsdata(request):
     if request.method == 'POST':
         fm = TeachersRegistration(request.POST)
-        print(fm)
-        print('This is POST statement')
-        print(fm.cleaned_data)
+        if fm.is_valid():
+            print(fm.cleaned_data['password'])
+            print(fm.cleaned_data['repassword'])
+            print('This is POST statement')
     else:
         fm = TeachersRegistration()
         print('This is GET statement')
     fm.order_fields(field_order=['email', 'first_name', 'last_name'])
-    return render(request, 'blogs/forms.html', {'form': fm})
+    return render(request, 'blogs/form.html', {'form': fm})
